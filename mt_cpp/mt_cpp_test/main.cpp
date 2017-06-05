3b23bf2c8655b12bfb40bb7a969187ab1036eb40
Viktor Kuznietsov X <viktor.x.kuznietsov@ericsson.com>
2017-06-05 13:24:29 -0400

    mt workspace + cpp project.

--- mt_cpp/mt_cpp_test/main.cpp
+++ mt_cpp/mt_cpp_test/main.cpp
@@ -1,7 +1,14 @@
 
 //#include "tas_lock.h"
+#include <thread>
+#include <iostream>
 
 int main() {
+
+	std::thread t([] {std::cout << "test" << std::endl; });
+	t.join();
+
+	std::cin.get();
 
 	return 0;
 }
