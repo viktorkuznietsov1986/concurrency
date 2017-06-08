9aec097e522fe0c0c286914b7d82b2f85fcabded
Viktor Kuznietsov X <viktor.x.kuznietsov@ericsson.com>
2017-06-08 18:49:30 -0400

    Added LockFreeStack + test.

---
+++ mt/src/collections/LockFreeStack.java
@@ -0,0 +1,85 @@
+package collections;
+
+import locks.Backoff;
+
+import java.util.EmptyStackException;
+import java.util.concurrent.atomic.AtomicReference;
+
+/**
+ * Created by evikuzn on 6/8/2017.
+ */
+public class LockFreeStack<T> implements Pool<T> {
+
+    class Node {
+        public T value;
+        public Node next;
+        public Node(T value) {
+            this.value = value;
+            this.next = null;
+        }
+    }
+
+    private AtomicReference<Node> top = new AtomicReference<>(null);
+    private static final int MIN_DELAY = 10;
+    private static final int MAX_DELAY = 1000;
+    private Backoff backoff = new Backoff(MIN_DELAY, MAX_DELAY);
+
+    @Override
+    public void set(T item) {
+        Node node = new Node(item);
+
+        while (true) {
+            if (tryPush(node)) {
+                return;
+            }
+            else {
+                try {
+                    backoff.backoff();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    @Override
+    public T get() {
+        while (true) {
+            Node n = tryPop();
+
+            if (n != null) {
+                return n.value;
+            }
+            else {
+                try {
+                    backoff.backoff();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    protected boolean tryPush(Node node) {
+        Node oldTop = top.get();
+        node.next = oldTop;
+        return top.compareAndSet(oldTop, node);
+    }
+
+    protected Node tryPop() throws EmptyStackException {
+        Node oldTop = top.get();
+
+        if (oldTop == null) {
+            throw new EmptyStackException();
+        }
+        else {
+            Node newTop = oldTop.next;
+            if (top.compareAndSet(oldTop, newTop)) {
+                return oldTop;
+            }
+            else {
+                return null;
+            }
+        }
+    }
+}
---
+++ mt/src/collections/LockFreeStackTest.java
@@ -0,0 +1,45 @@
+package collections;
+
+import org.junit.Test;
+
+/**
+ * Created by evikuzn on 6/8/2017.
+ */
+public class LockFreeStackTest {
+    @Test
+    public void testLockFreeStack() {
+        Pool<Integer> p = new LockFreeStack<>();
+
+        Thread t1 = createSetThread(p, 1);
+        Thread t2 = createSetThread(p, 2);
+        Thread t3 = createSetThread(p, 3);
+        Thread t4 = createSetThread(p, 4);
+        Thread t5 = createSetThread(p, 5);
+        Thread t6 = createSetThread(p, 6);
+
+        Thread t7 = createGetThread(p);
+        Thread t8 = createGetThread(p);
+        Thread t9 = createGetThread(p);
+
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+        t5.start();
+        t6.start();
+        t7.start();
+        t8.start();
+        t9.start();
+    }
+
+    private Thread createSetThread(Pool<Integer> p, int value) {
+        return new Thread(() -> {
+            System.out.println("LockFreeStack.set " + value);
+            p.set(value);
+        });
+    }
+
+    private Thread createGetThread(Pool<Integer> p) {
+        return new Thread(() -> { System.out.println("LockFreeStack.get " + p.get()); });
+    }
+}
